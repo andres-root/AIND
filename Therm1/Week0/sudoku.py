33f8364095788d4e8e5a538f145ad2694dc1f6bc
Andres Lujan <andres.root.coder@gmail.com>
2017-02-20 11:17:03 -0500

    Adding resolver function

--- Therm1/Week0/sudoku.py
+++ Therm1/Week0/sudoku.py
@@ -14,34 +14,65 @@ class Sudoku:
         ]
         self.unitlist = self.row_units + self.column_units + self.square_units
         self.units = dict((s, [u for u in self.unitlist if s in u]) for s in self.boxes)
-        self.peers = dict((s, set(sum(self.units[s],[]))-set([s])) for s in self.boxes)
+        self.peers = dict((s, set(sum(self.units[s], [])) - set([s])) for s in self.boxes)
+        self.values = None
+        self.stalled = False
 
     def cross(self, a, b):
         return [s + t for s in a for t in b]
 
-    def grid_values(self, unsolved):
-        unsolved = dict(zip(self.boxes, unsolved))
-        unsolved = {k: self.cols if v == '.' else v for (k, v) in unsolved.items()}
-        return unsolved
+    def grid_values(self, values):
+        values = dict(zip(self.boxes, values))
+        values = {k: self.cols if v == '.' else v for (k, v) in values.items()}
+        return values
 
-    def eliminate(self, unsolved):
-        solved_boxes = [box for box in unsolved.keys() if len(unsolved[box]) == 1]
+    def eliminate(self, values):
+        if not values:
+            values = self.values
+
+        solved_boxes = [box for box in values.keys() if len(values[box]) == 1]
         for box in solved_boxes:
-            n = unsolved[box]
+            n = values[box]
             for peer in self.peers[box]:
-                unsolved[peer] = unsolved[peer].replace(n, '')
-        return unsolved
+                values[peer] = values[peer].replace(n, '')
+        return values
 
-    def display(self, unsolved):
+    def one_choice(self, values):
+        if not values:
+            values = self.values
+
+        for unit in self.unitlist:
+            for n in '123456789':
+                boxes = [box for box in unit if n in values[box]]
+                if len(boxes) == 1:
+                    values[boxes[0]] = n
+        return values
+
+    def reduce_puzzle(self, values):
+        if not values:
+            values = self.values
+
+        while not self.stalled:
+            # Check how many boxes have a determined value
+            solved_values_before = len([box for box in values.keys() if len(values[box])])
+
+            # Eliminate strategy
+            self.eliminate()
+
+            # One choice strategy
+            self.one_choice()
+
+        return
+
+    def display(self, values):
         """
         Display the values as a 2-D grid.
         Input: The sudoku in dictionary form
         Output: None
         """
-        values = self.grid_values(unsolved)
-        values = self.eliminate(values)
-        # print(values)
-        # return False
+        if not values:
+            values = self.values
+
         width = 1 + max(len(values[s]) for s in self.boxes)
         line = '+'.join(['-' * (width * 3)] * 3)
         for r in self.rows:
@@ -51,10 +82,13 @@ class Sudoku:
                 print(line)
         return
 
-    def solve(self):
+    def solve(self, values):
+        self.values = values
+        self.reduce_puzzle()
         return True
+
 
 if __name__ == '__main__':
     sudoku = Sudoku()
     print(sudoku.display('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))
-    import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
