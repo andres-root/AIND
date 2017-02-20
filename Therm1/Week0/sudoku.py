eb7e247bec9b694c0d72019af1b5ceb9ffb760ba
Andres Lujan <andres.root.coder@gmail.com>
2017-02-20 15:07:51 -0500

    working on search three issues

--- Therm1/Week0/sudoku.py
+++ Therm1/Week0/sudoku.py
@@ -23,6 +23,7 @@ class Sudoku:
 
     def grid_values(self, values):
         values = dict(zip(self.boxes, values))
+        self.display(values)
         values = {k: self.cols if v == '.' else v for (k, v) in values.items()}
         return values
 
@@ -42,10 +43,7 @@ class Sudoku:
                     values[boxes[0]] = n
         return values
 
-    def reduce_puzzle(self, values=None):
-        if not values:
-            values = self.values
-
+    def reduce_puzzle(self, values):
         while not self.stalled:
             # Check how many boxes have a determined value
             solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
@@ -65,6 +63,20 @@ class Sudoku:
                 return False
         return values
 
+    def search(self, values):
+        values = self.reduce_puzzle(values)
+        if values is False:
+            return False
+        if all(len(values[s]) == 1 for s in self.boxes):
+            return values
+        n, s = min((len(values[s]), s) for s in self.boxes if len(values[s]) > 1)
+        for value in values[s]:
+            new_sudoku = values.copy()
+            new_sudoku[s] = value
+            attempt = self.search(new_sudoku)
+            if attempt:
+                return attempt
+
     def display(self, values=None):
         """
         Display the values as a 2-D grid.
@@ -81,18 +93,21 @@ class Sudoku:
             print(g)
             if r in 'CF':
                 print(line)
-        return
+        print('----------------------------------------------------------------')
 
     def solve(self, values, display=False):
         self.values = self.grid_values(values)
-        self.reduce_puzzle()
+        self.values = self.reduce_puzzle(self.values)
+        self.values = self.search(self.values)
         if display is True:
             self.display()
-        return self.values
+        else:
+            return self.values
 
 
 if __name__ == '__main__':
     sudoku = Sudoku()
-    unsolved = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+    # unsolved = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+    unsolved = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
     print(sudoku.solve(unsolved, display=True))
     # import ipdb; ipdb.set_trace()
