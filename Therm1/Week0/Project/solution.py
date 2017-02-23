7ae3dece8594ae4ce5ecfc515269a6154012fc86
Andres Lujan <andres.root.coder@gmail.com>
2017-02-23 11:48:08 -0500

    added comments

--- Therm1/Week0/Project/solution.py
+++ Therm1/Week0/Project/solution.py
@@ -4,6 +4,11 @@ from visualize import visualize_assignments
 class Sudoku:
 
     def __init__(self, diagonal=False):
+        """
+        Constructor
+        Input: diagonal Bool. Indicates it the sudoku is diagonal
+        Output: None
+        """
         self.rows = 'ABCDEFGHI'
         self.cols = '123456789'
         self.boxes = self.cross(self.rows, self.cols)
@@ -33,9 +38,15 @@ class Sudoku:
         return values
 
     def cross(self, a, b):
+        """
+        Cross product of elements in A and elements in B.
+        """
         return [s + t for s in a for t in b]
 
     def diagonal(self, a, b):
+        """
+        Returns the two diagonal units
+        """
         rows = list(a)
         cols = list(b)
         left_diag = []
@@ -51,11 +62,29 @@ class Sudoku:
         return [left_diag, right_diag]
 
     def grid_values(self, values):
+        """
+        Convert grid into a dict of {square: char} with '123456789' for empties.
+        Args:
+            grid(string) - A grid in string form.
+        Returns:
+            A grid in dictionary form
+                Keys: The boxes, e.g., 'A1'
+                Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
+        """
         values = dict(zip(self.boxes, values))
         values = {k: self.cols if v == '.' else v for (k, v) in values.items()}
         return values
 
     def eliminate(self, values, n=None, value=None):
+        """
+        Strategy 1: Elimination
+        If a box has a value assigned, then none of the peers of this box can
+        have this value.
+        Args:
+            values(dict): a dictionary of the form {'box_name': '123456789', ...}
+        Returns:
+            the values dictionary with the naked twins eliminated from peers.
+        """
         solved_boxes = [box for box in values.keys() if len(values[box]) == 1]
         for box in solved_boxes:
             n = values[box]
@@ -64,6 +93,11 @@ class Sudoku:
         return values
 
     def one_choice(self, values):
+        """
+        Strategy 2: Only Choice
+        If there is only one box in a unit which would allow a certain digit,
+        then that box must be assigned that digit.
+        """
         for unit in self.unitlist:
             for n in '123456789':
                 boxes = [box for box in unit if n in values[box]]
@@ -72,6 +106,15 @@ class Sudoku:
         return values
 
     def naked_twins(self, values):
+        """
+        Strategy 3: Naked Twins
+        If two boxes in the same unit have only two values and they are equal
+        then we remove every of those values for every peer in that unit
+            Args:
+                values(dict): a dictionary of the form {'box_name': '123456789', ...}
+            Returns:
+                the values dictionary with the naked twins eliminated from peers.
+        """
         unit_values = [{box: values[box] for box in unit} for unit in self.unitlist]
         for unit in unit_values:
             twins = {k: v for (k, v) in unit.items() if len(v) == 2 if list(unit.values()).count(v) > 1}
@@ -86,6 +129,11 @@ class Sudoku:
         return values
 
     def reduce_puzzle(self, values):
+        """
+        Combines each strategy to find a solution.
+        If a solution is found we return the values.
+        If no solution is found we stop trying.
+        """
         stalled = False
         while not stalled:
             # Check how many boxes have a determined value
@@ -110,6 +158,11 @@ class Sudoku:
         return values
 
     def search(self, values):
+        """
+        Strategy 3: Search
+        Pick a box with a minimal number of possible values.
+        Try to solve each of the puzzles obtained by choosing each of these values, recursively.
+        """
         values = self.reduce_puzzle(values)
         if values is False:
             return False
@@ -126,8 +179,9 @@ class Sudoku:
     def display(self, values, gui=False):
         """
         Display the values as a 2-D grid.
-        Input: The sudoku in dictionary form
-        Output: None
+        Args:
+            values(dict): The sudoku in dictionary form
+            gui(bool): Flag that indicates if gui should be shown
         """
         try:
             width = 1 + max(len(values[s]) for s in self.boxes)
@@ -145,8 +199,16 @@ class Sudoku:
             print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
         return
 
-    def solve(self, values, display=False, gui=False):
-        self.values = self.grid_values(values)
+    def solve(self, grid, display=False, gui=False):
+        """
+        Find the solution to a Sudoku grid.
+        Args:
+            grid(string): a string representing a sudoku grid.
+                Example: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+        Returns:
+            The dictionary representation of the final sudoku grid. False if no solution exists.
+        """
+        self.values = self.grid_values(grid)
         self.values = self.reduce_puzzle(self.values)
         self.values = self.search(self.values)
         if display is True:
