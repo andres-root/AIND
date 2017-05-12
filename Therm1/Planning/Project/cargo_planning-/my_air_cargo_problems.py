4a869b850c69a68ddea6c2e7aed10289329b9382
Andres Root <info@andresroot.co>
2017-05-12 15:25:14 -0500

    working on corrections

--- Therm1/Planning/Project/cargo_planning-/my_air_cargo_problems.py
+++ Therm1/Planning/Project/cargo_planning-/my_air_cargo_problems.py
@@ -283,7 +283,7 @@ def air_cargo_p2() -> AirCargoProblem:
 
 def air_cargo_p3() -> AirCargoProblem:
     cargos = ['C1', 'C2', 'C3', 'C4']
-    planes = ['P1', 'P2']
+    planes = ['P1', 'P2', 'P3', 'P4']
     airports = ['JFK', 'SFO', 'ATL', 'ORD']
     pos = [expr('At(C1, SFO)'),
            expr('At(C2, JFK)'),
