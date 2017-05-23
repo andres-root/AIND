5cc6a6e98b32f9087b0ba788129c42a98984e702
Andres Root <info@andresroot.co>
2017-05-22 23:00:17 -0500

    Working on ASL Recognizer project

--- Therm1/ASL-Recognizer/my_recognizer.py
+++ Therm1/ASL-Recognizer/my_recognizer.py
@@ -11,8 +11,8 @@ def recognize(models: dict, test_set: SinglesData):
    :return: (list, list)  as probabilities, guesses
        both lists are ordered by the test set word_id
        probabilities is a list of dictionaries where each key a word and value is Log Liklihood
-           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
-            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
+           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD': LogLvalue, ... },
+            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD': LogLvalue, ... },
             ]
        guesses is a list of the best guess words ordered by the test set word_id
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
@@ -20,6 +20,20 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    test_sequences = list(test_set.get_all_Xlengths().values())
+    try:
+        for test_X, test_length in test_sequences:
+            best_score = float('-inf')
+            best_word = None
+            logL_dict = {}
+            for word, model in models.items():
+                logL = model.score(test_X, test_length)
+                logL_dict[word] = logL
+                if logL > best_score:
+                    best_score = logL
+                    best_word = word
+            probabilities.append(logL_dict)
+            guesses.append(best_word)
+    except:
+        pass
+    return probabilities, guesses
